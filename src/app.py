6366c244b94180f6b2f80b0dc7f4298ce7674f4d
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-06-12 23:25:37 -0400

    frondend.py

--- src/app.py
+++ src/app.py
@@ -1,25 +1,28 @@
+from .frontend import login_module, register_module, success_login, exit_app
+
 class App:
 
-    page_number = 0 
+    def __init__(self):
+        self.page_number = 0 
 
     def run(self):
 
         while True:
-            
-            print("1 For Login, 2 For Register, 3 For Exit: \n")
+
+            print("pageNum", self.page_number)
+            print("1 For Login, 2 For Register, 3 For Exit: ")
             self.page_number = int(input())
 
             if self.page_number == 1:
-                print("LOGIN")
+                login_module()
             elif self.page_number == 2:
-                print("REGISTER")
+                register_module()
+            elif self.page_number == 9:
+                success_login()
             elif self.page_number == 3:
-                break 
-            
-            
+                exit_app()
 
     def change_page(self, new_page_num):
         self.page_number = new_page_num
     
-    
-    
+
---
+++ src/frontend.py
@@ -0,0 +1,21 @@
+def success_login():
+    print("LOGIN SUCCESS")
+
+def login_module():
+
+    def module_tilte():
+        print("LOGIN MODULE")
+
+    module_tilte()
+    username = input("USERNAME: ")
+    password = input("PASSWORD: ") 
+
+
+
+
+def register_module():
+    print("REGISTER")
+
+def exit_app():
+    print("LOGOUT SUCCESSFUL")
+    quit()
