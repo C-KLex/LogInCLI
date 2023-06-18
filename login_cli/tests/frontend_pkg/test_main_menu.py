25361ed2ca55fdef4dd0b9e5604d7b0818d32fbc
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-06-17 22:44:25 -0400

    test: frontend_main_menu

---
+++ login_cli/tests/frontend_pkg/test_main_menu.py
@@ -0,0 +1,31 @@
+from unittest.mock import patch, MagicMock
+import pytest
+from login_cli.frontend.frontend import Frontend
+
+@pytest.fixture
+def frontend():
+    return Frontend()
+
+def test_main_menu_page_with_valid_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='1'))
+
+    choice = frontend.main_menu_page()
+
+    assert choice == 1
+
+def test_main_menu_page_with_invalid_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='4'))
+
+    choice = frontend.main_menu_page()
+
+    assert choice == 4
+
+def test_main_menu_page_with_nonnumeric_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='abc'))
+
+    with pytest.raises(ValueError):
+        frontend.main_menu_page()
+
