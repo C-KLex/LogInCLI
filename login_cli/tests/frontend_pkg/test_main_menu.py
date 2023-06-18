ca5d90f4bafe37f41cb62792601447dc01ccef95
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-06-18 00:31:42 -0400

    test: build test case on frontend main_menu after logged in

--- login_cli/tests/frontend_pkg/test_main_menu.py
+++ login_cli/tests/frontend_pkg/test_main_menu.py
@@ -29,3 +29,26 @@ def test_main_menu_page_with_nonnumeric_choice(frontend, monkeypatch, capsys):
     with pytest.raises(ValueError):
         frontend.main_menu_page()
 
+def test_main_menu_page_logged_with_valid_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='1'))
+
+    choice = frontend.main_menu_after_logged_page()
+
+    assert choice == 1
+
+def test_main_menu_page_logged_with_invalid_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='4'))
+
+    choice = frontend.main_menu_after_logged_page()
+
+    assert choice == 4
+
+def test_main_menu_page_logged_with_nonnumeric_choice(frontend, monkeypatch, capsys):
+    # Mock the input value
+    monkeypatch.setattr('builtins.input', MagicMock(return_value='abc'))
+
+    with pytest.raises(ValueError):
+        frontend.main_menu_after_logged_page()
+
