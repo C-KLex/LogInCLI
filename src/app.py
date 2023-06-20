db5797a03b50d90403e801b38b0d03b2d5df5f06
Ziqi Lin <ziqilin329@gmail.com>
2023-06-20 10:38:37 -0400

    read backend

--- src/app.py
+++ src/app.py
@@ -1,2 +1,18 @@
 def main():
-    print("HELLOW WORLD!")
+    usernames = []
+    passwords = []
+
+    with open("backend.csv", "a+", encoding="utf-8") as be:
+        be.seek(0)
+        res = be.readlines()
+        for i in res:
+            r = i.strip()
+            arr = r.split(':')
+            usernames.append(arr[0])
+            passwords.append(arr[1])
+
+    registered = True
+    while registered:
+        username = input("Please enter the username: ")
+        if username in usernames:
+            print("")
